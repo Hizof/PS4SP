86c450150e02963081963c72959092015bfbfd5c
Hizof <ddbibin@yandex.ru>
2016-01-21 12:21:02 +0300

    Create SPSolution.ps1

---
+++ SPSolution.ps1
@@ -0,0 +1,22 @@
+#Download all wsp from farm
+
+$dirName = "<directory path>" 
+Write-Host Exporting solutions to $dirName  
+
+foreach ($solution in Get-SPSolution)  
+{  
+    $id = $Solution.SolutionID  
+    $title = $Solution.Name  
+    $filename = $Solution.SolutionFile.Name 
+
+    Write-Host "Exporting ‘$title’ to …\$filename" -nonewline  
+
+    try {  
+        $solution.SolutionFile.SaveAs("$dirName\$filename")  
+        Write-Host " – done" -foreground green  
+    }  
+    catch  
+    {  
+        Write-Host " – error : $_" -foreground red  
+    }  
+}
