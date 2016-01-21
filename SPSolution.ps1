c8f1c427bd6e6b5706e209fe063fb9bd8ab61d3f
Hizof <ddbibin@yandex.ru>
2016-01-21 12:26:01 +0300

    Update SPSolution.ps1

--- SPSolution.ps1
+++ SPSolution.ps1
@@ -20,3 +20,50 @@ foreach ($solution in Get-SPSolution)
         Write-Host " – error : $_" -foreground red  
     }  
 }
+
+#Add multiple farm solutions using power shell (Not Deploy)
+#http://sharepoint.stackexchange.com/questions/38203/add-multiple-farm-solutions-using-power-shell-not-deploy
+
+Remove-PSSnapin Microsoft.SharePoint.PowerShell -erroraction SilentlyContinue
+Add-PSSnapin Microsoft.SharePoint.PowerShell -erroraction SilentlyContinue
+function WaitForInsallation([string] $Name)
+{
+        Write-Host -NoNewline "Waiting for deployment job to complete" $Name "."
+        $wspSol = get-SpSolution $Name
+        while($wspSol.JobExists)
+        {
+            sleep 2
+            Write-Host -NoNewline "."
+            $wspSol = get-SpSolution $Name
+        }
+        Write-Host "job ended" -ForegroundColor green
+}
+Function Deploy-SPSolution ($WspFolderPath)
+{
+    $wspFiles = get-childitem $WspFolderPath | where {$_.Name -like "*.wsp"}
+
+    ForEach($file in $wspFiles)
+    {
+        $wsp = Get-SPSolution | Where{$_.Name -eq $file.Name}
+        if($wsp -eq $null)
+        {
+            write-host "Adding solution"
+            Add-SPSolution -LiteralPath ($WspFolderPath + "\" + $file.Name)
+        }
+        else
+        {
+            write-host "solution already exists"
+
+        }
+
+    }
+}
+try
+{
+        Deploy-SPSolution "C:\EXPORTEDWSP"
+}
+catch
+{
+    write-host $_.exception
+
+}
