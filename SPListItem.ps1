21e1e6240b91fd73a5bc46bbc90fb546e3de5108
Hizof <ddbibin@yandex.ru>
2016-02-09 11:54:20 +0300

    Update SPListItem.ps1

--- SPListItem.ps1
+++ SPListItem.ps1
@@ -6,5 +6,24 @@ $i = $list.GetItemById(9000)
 $i.Fields | 
     ?{ $_.Hidden -eq $false -and $_.ReadOnlyField -eq $false -and $_.Title -ne "Attachments" } | 
     %{@{($_.Title + " (" + $_.InternalName + ")") = $i[$_.InternalName]}} #| Out-GridView
+
+#Query
+$spQuery = New-Object Microsoft.SharePoint.SPQuery
+$spQuery.ViewAttributes = "Scope='Recursive'";
+$spQuery.RowLimit = 2000
+$caml = '<Query><Where><IsNull><FieldRef Name="Title" /></IsNull></Where></Query>' 
+$spQuery.Query = $caml 
+
+do
+{
+    $listItems = $list.GetItems($spQuery)
+    $spQuery.ListItemCollectionPosition = $listItems.ListItemCollectionPosition
+    foreach($item in $listItems)
+    {
+        Write-Host $item.Title
+    }
+}
+while ($spQuery.ListItemCollectionPosition -ne $null)
+
     
 $web.Dispose()
