255c641259140b06ee7b9143250e6c41a4820072
Hizof <ddbibin@yandex.ru>
2016-02-20 13:28:51 +0300

    Update SPFeature.ps1

--- SPFeature.ps1
+++ SPFeature.ps1
@@ -4,4 +4,17 @@ function Reactivate-SPFeature($Identity, $url, $Confirm)
 	Enable-SPFeature –Identity $Identity –url $url
 }
 
+function Get-SPFeatureActivateStatus ($Identity, $url)
+{
+	$feature = Get-SPFeature –Identity $Identity –Web $url -ErrorAction SilentlyContinue
+	if($feature -ne $null)
+	{
+		return $true
+	}
+	else
+	{
+		return $false
+	}
+}
+
 Reactivate-SPFeature "5a578ec2-a66c-491f-a71f-e61e6a78e413" $web.Url $false
