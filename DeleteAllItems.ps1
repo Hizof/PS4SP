d0b465224ad56c950a1a741e556a87e7e07a36f4
Hizof <ddbibin@yandex.ru>
2016-03-09 12:49:11 +0300

    Update DeleteAllItems.ps1

--- DeleteAllItems.ps1
+++ DeleteAllItems.ps1
@@ -8,6 +8,7 @@ function Clear-SPList($list)
         $list.GetItemById($Item.id).Delete()
     }
 }
+
 function Clear-SPList($list)
 {
     #variant B
@@ -18,4 +19,37 @@ function Clear-SPList($list)
     {
        $items[$intIndex].Delete();
     }
+}
+
+function Clear-SPListBatch($list, $rowLimit = 500, $camlQuery = "")
+{
+    $query = new-object Microsoft.Sharepoint.SPQuery   
+    $query.Query = $camlQuery
+    $query.RowLimit = $rowLimit
+	
+    do
+    {
+		$items = $list.GetItems($query)
+		$query.ListItemCollectionPosition = $Items.ListItemCollectionPosition
+		Write-Host $query.ListItemCollectionPosition
+		$count = $items.Count -1;
+		Write-Host $count
+
+		# http://www.theodells.org/theodells/blog/2012/10/powershell-function-to-delete-all-sharepoint-list-items/
+		[System.Text.StringBuilder]$batchXml = New-Object "System.Text.StringBuilder"; 
+		$batchXml.Append("<?xml version=`"1.0`" encoding=`"UTF-8`"?><Batch>")
+		$cmd = "<Method><SetList Scope=`"Request`">" + $list.ID + "</SetList><SetVar Name=`"ID`">{0}</SetVar><SetVar Name=`"Cmd`">Delete</SetVar></Method>"
+		
+		for($intIndex = $count; $intIndex -ge 0; $intIndex--)
+		{
+		   if($items[$intIndex] -ne $null)
+		   {
+				$batchString = [System.String]::Format($cmd, $items[$intIndex].ID.ToString())
+				$batchXml.Append($batchString)
+		   }
+		}
+		$batchXml.Append("</Batch>");
+		$web.ProcessBatchData($batchXml.ToString())|Out-Null
+    }
+	while($query.ListItemCollectionPosition -ne $null)
 }
