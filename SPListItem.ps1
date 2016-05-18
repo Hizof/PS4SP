9629de640be7a263b441abd1d717c92f55395012
Hizof <ddbibin@yandex.ru>
2016-05-18 13:18:22 +0300

    Update SPListItem.ps1

--- SPListItem.ps1
+++ SPListItem.ps1
@@ -7,6 +7,22 @@ $i.Fields |
     ?{ $_.Hidden -eq $false -and $_.ReadOnlyField -eq $false -and $_.Title -ne "Attachments" } | 
     %{@{($_.Title + " (" + $_.InternalName + ")") = $i[$_.InternalName]}} #| Out-GridView
 
+#Copy SPListItem to new SPList
+function Copy-SPListItem($sourceItem, $destinationList)
+{
+    $destinationItem = $destinationList.AddItem()
+    
+    $destinationItem["Title"] = $sourceItem["Title"]
+
+    $sourceItem.Fields |?{$_.SourceId -ne "http://schemas.microsoft.com/sharepoint/v3"}| %{
+        $destinationItem[$_.InternalName] = $sourceItem[$_.InternalName]
+    }
+
+    $destinationItem.Update()
+
+    return $destinationItem
+}
+
 #Query
 $spQuery = New-Object Microsoft.SharePoint.SPQuery
 $spQuery.ViewAttributes = "Scope='Recursive'";
